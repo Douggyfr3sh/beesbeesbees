2a0748afa1bb8aacc3f16822bfd2373ec91745f1
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-22 14:30:43 -0400

    Implemented the Grub class.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
 var Grub = function() {
+  //this = Object.create(Grub.prototype)
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+  //retval is this;
 };
 
+Grub.prototype.eat = function () {
+
+};
