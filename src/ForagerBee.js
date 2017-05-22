0310e7178de83fba16ebc2210b277dc8cfd83aa8
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-22 14:52:59 -0400

    Implemented ForagerBee and HoneyMakerBee subclasses.

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,22 @@
 var ForagerBee = function() {
+  //call Bee superclass
+  Bee.call(this);
+
+  this.age = 10;
+  this.job = 'find pollen';
+  //color, food, eat are inherited
+  this.canFly = true;
+  this.treasureChest = [];
 };
+
+//set the prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+
+//set the constructor
+ForagerBee.prototype.constructor = ForagerBee;
+
+//write forage method
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,30 @@
 var HoneyMakerBee = function() {
+  //call the Bee superclass
+  Bee.call(this);
+  //set age to 10
+  this.age = 10;
+  //set job to 'make honey'
+  this.job = 'make honey';
+  //color is inherited from Bee, food and eat inherited from Grub
+  //(nothing to code for this)
+
+  //create honeyPot property equal to 0
+  this.honeyPot = 0;
+
 };
+
+//set the prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+//set the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+//makeHoney method - increment honeyPot
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+}
+
+//giveHoney method, decrement honeyPot
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot > 0 ? this.honeyPot-- : this.honeyPot = this.honeyPot;
+}
 
