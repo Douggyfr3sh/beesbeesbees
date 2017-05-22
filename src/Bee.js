1db1b28f6014a4e71e0fb6518c3ead8f707fadb1
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-05-22 14:40:01 -0400

    Implemented Bee class.

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,18 @@
 var Bee = function() {
+  //create the object using Grub as it's superclass
+  Grub.call(this);
+  this.age =  5;
+  this.color =  'yellow';
+  //food, eat inherited from Grub
+  this.job = 'keep on growing';
 };
+
+//subclass extends superclass
+
+//set prototype
+Bee.prototype = Object.create(Grub.prototype);
+//set constructor
+Bee.prototype.constructor = Bee;
+
+
 
